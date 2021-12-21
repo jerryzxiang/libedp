5abdc64bd304ac9e93d2b035c3f9167a36e5814b
jzxiang <jzxiang@princeton.edu>
2021-12-21 17:02:46 -0500

    added neware_reader files

---
+++ neware_reader-master/README.rst
@@ -0,0 +1,14 @@
+Neware reader
+
+This Python Package was developed to help researchers to extract data from Neware battery analyzers.
+
+Free software: BSD-2 license
+
+Features
+
+read_nda(),
+csv_to_nda()
+
+Credits
+
+This is a collaborative work between Beyonder (http://beyonder.no/home), Corvus Energy (https://corvusenergy.com/), and IFE (http://ife.no/en/).
---
+++ neware_reader-master/neware.py
@@ -0,0 +1,47 @@
+#!/bin/python3.4
+import pandas as pd
+import os
+#from . import new_nda, old_nda, nda_version_8_0
+import new_nda, old_nda, nda_version_8_0
+
+def read_nda(inpath, testcols=False, split=False, small=False):
+    # inpath: 'yourfile.nda'
+    # testcols: columns of data that are empty in my files, but might contain data (e.g. temperature).
+    # split: in newer nda files capacity and energy are in separate columns depending on the direction of the current.
+    # split=False outputs a single column for each (one for capacity and one for energy), while split=True outputs two
+    # for each (chg_capacity_mAh, dchg_capacity_mAh, chg_energy_mWh, and dchg_energy_mWh)
+    # small: converts the data from float64 to float32. small=True also omits the 'timestamp', as well as returning numbers
+    # instead of strings for 'step_name'. small=True tends to reduce the size of the dataframe by 50-70%
+
+    with open(inpath, 'rb') as f:
+        data = f.read()
+
+    if data[112:115] == b'BTS':
+        outdata = old_nda.old_nda(inpath, testcols=testcols, split=split, small=small)
+    elif data[2693:2706]==b'BTS Client 8.':
+        outdata = nda_version_8_0.nda_version_8_0(inpath, testcols=testcols, split=split, small=small)
+    else:
+        outdata = new_nda.new_nda(inpath, testcols=testcols, split=split, small=small)
+
+    return outdata
+
+def nda_to_csv(inpath, outpath=':auto:', testcols=False, split=False, small=False):
+#    process_nda(inpath)
+
+    if outpath == ':auto:':
+        outpath = inpath + '.csv'
+
+    df = read_nda(inpath, testcols=testcols, split=split, small=small)
+    df.to_csv(outpath)
+    
+user_filepath = r'C:\\Users\\jerry\\' #change username based on computer
+
+backup_files_folder = user_filepath + 'Box\PNE - R&D (Xiaofang)\R&D Adminstration\Projects\E-chem\E-chem data (auto backup)'
+outpath = backup_files_folder + ' - Excel'
+
+nda_files = os.listdir(backup_files_folder)
+
+for item in nda_files:
+    nda_to_csv(backup_files_folder + "\\" + item)
+    
+
