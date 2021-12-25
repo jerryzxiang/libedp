80a3c9cd016d272e8368b206ebd8c5333360daf9
jerryzxiang <jzxiang@jzxiang.localdomain>
2021-12-25 10:01:47 -0500

    created libdataexport class and first function.

---
+++ data_export/libdataexport.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from pandas import DataFrame
+
+class libdataexport():
+    def __init__(self):
+        self = self
+    
+    def export2csv(self, file, filename, mass, label):
+        df = DataFrame({'Label': label, 'Active Mass':
+                        mass})
+        df = pd.DataFrame.from_dict(df, orient='index')
+        df = df.transpose()
+        df.to_excel(filename + " data_export.xlsx", sheet_name='sheet1',
+                       index=False)
+        
