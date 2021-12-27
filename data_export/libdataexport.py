e2e6ad36feb729fd7e290fcb76fb0729783e9a2d
jerryzxiang <jzxiang@jzxiang.localdomain>
2021-12-27 00:54:46 -0500

    added exportScript. added function getSpecDisCapacity to libdataexport

--- data_export/libdataexport.py
+++ data_export/libdataexport.py
@@ -15,4 +15,15 @@ class libdataexport():
         df = df.transpose()
         df.to_excel(filename + " data_export.xlsx", sheet_name='sheet1',
                        index=False)
+    
+    def getSpecDisCapacity(file, filename, mass, label):
+        df = pd.ExcelFile(file).parse(sheet_name = 1)
+        # Cycle Number
+        cycNum = df['ToTal of Cycle']
+        # Discharge Capacity
+        disCap = df['Capacity of discharge(mAh)']
+        # Specific Discharge Capacity
+        specDisCap = disCap / mass 
+
+        return specDisCap
         
